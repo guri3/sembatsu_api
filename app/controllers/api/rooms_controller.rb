e44092472e6a714175352b83642dfdd6b19208ff
guri3 <aloesquash.lock@gmail.com>
2018-10-26 22:28:49 +0900

    暫定コミット

--- app/controllers/api/rooms_controller.rb
+++ app/controllers/api/rooms_controller.rb
@@ -2,28 +2,26 @@ class Api::RoomsController < ApplicationController
   def index
     @rooms = Room.all
 
-    # カテゴリごとの部屋を抜き出す必要があったら処理を行う
-    # 現状全て同じものを返している
-    if params[:keyword].present?
-      case params[:keyword]
-      when 'scenery'
-        render json: @rooms
-      when 'inexpensive'
-        render json: @rooms
-      when 'accessible'
-        render json: @rooms
-      when 'satisfaction'
-        render json: @rooms
-      when 'clean'
-        render json: @rooms
-      when 'equipment'
-        render json: @rooms
-      else
-        render json: @rooms
-      end
-    else
-      render 'index', formats: 'json', handlers: 'jbuilder'
-    end
+    # if params[:keyword].present?
+    #   case params[:keyword]
+    #   when 'scenery'
+    #     render json: @rooms
+    #   when 'inexpensive'
+    #     render json: @rooms
+    #   when 'accessible'
+    #     render json: @rooms
+    #   when 'satisfaction'
+    #     render json: @rooms
+    #   when 'clean'
+    #     render json: @rooms
+    #   when 'equipment'
+    #     render json: @rooms
+    #   else
+    #     render json: @rooms
+    #   end
+    # else
+    render 'index', formats: 'json', handlers: 'jbuilder'
+    # end
   end
 
   def show
