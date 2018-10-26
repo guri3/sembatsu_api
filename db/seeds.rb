a919589fbe26609a4c1c91ed48135ef086515ac4
guri3 <aloesquash.lock@gmail.com>
2018-10-27 03:58:11 +0900

    少し宿情報を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -67,13 +67,23 @@ Option.create(
   ]
 )
 
-body = <<~EOS
+body1 = <<~EOS
+箱根の奥座敷、豊富な水を湛えた芦ノ湖と雄大な自然が五感に囁きかける。
+心地よいぬくもりに身を委ねれば花がほころぶようにふわりと心解き放たれる。
+EOS
+body2 = <<~EOS
 全てのナチュラル&マッドウォールハウス！立石東京駅周辺の綺麗な近所。
 多くのバーは、戦争直後から駅の近くに残っています。
 1940年代に東京がどのようなものだったかを感じることができます。
 主要スポットへのアクセス良好(15分~50分)
 EOS
-child = <<~EOS
+body3 = <<EOS
+都心にありながら、芝公園の豊かな緑に囲まれています。2018年3月リニューアル。大切な方と上質で特別な時間をお過ごしください。
+EOS
+body4 = <<~EOS
+那覇空港から車で約50分。沖縄でも有数の美しい観光スポットのほぼ中心。マリーナへも近く、マリンスポーツもより身近に楽しめるロケーション。近隣にはゴルフ場が控え、レジャースポットとしてのポテンシャルの高さを誇ります。沖縄を知る人の憧れでもあるこの地で素敵な時間を過ごしてみませんか？
+EOS
+child1 = <<~EOS
 ご使用いただけます。
 12歳以下のお子様は一律1,620円(税込)の施設利用料が掛かります。
 子供料金は、12歳以下の施設利用料1,620円に下記料金を加算しております。
@@ -83,9 +93,16 @@ child = <<~EOS
 ※添い寝は0～12歳（小学生）までご利用いただけます。13歳以上（中学生）は大人でご予約下さい。
 ※新棟「月の道」は6歳以上のご利用となります。
 EOS
-access = <<~EOS
+access1 = <<~EOS
 バス停まで歩いておよそ17分。
 駐車場は無料です。6台は停められる余裕があります。
+EOS
+access2 = <<~EOS
+バス停まで歩いておよそ10分。
+EOS
+access3 = <<~EOS
+最寄り駅まで徒歩5分
+駐車場は無料です。6台分用意してあります。
 EOS
 
 host_id = Host.first.id
@@ -95,8 +112,8 @@ Room.create(
       host_id: host_id,
       title: '湘南の海が一望できる別荘で素敵な休日を過ごしませんか？',
       registration_id: 'M0000001',
-      body: body,
-      image_url: 'https://ateamateam.herokuapp.com/images/sample_room.jpg',
+      body: body1,
+      image_url: 'https://ateamateam.herokuapp.com/images/room1.jpg',
       prefecture: '神奈川県',
       city: '箱根',
       latitude: 35.239728,
@@ -117,10 +134,10 @@ Room.create(
     },
     {
       host_id: host_id,
-      title: '湘南の海が一望できる別荘で素敵な休日を過ごしませんか？',
+      title: '広大な自然に包まれ心からリラックスできる時間を',
       registration_id: 'M0000002',
       body: body,
-      image_url: 'https://ateamateam.herokuapp.com/images/sample_room.jpg',
+      image_url: 'https://ateamateam.herokuapp.com/images/room2.jpg',
       prefecture: '神奈川県',
       city: '箱根',
       latitude: 35.239728,
@@ -136,7 +153,7 @@ Room.create(
       amenity: 'シャンプー・リンス,ボディソープ・石鹸,ボディタオル,ハミガキセット,シャワーキャップ,ドライヤー',
       pet: 'ご一緒に宿泊できます。',
       child: child,
-      access: access,
+      access: access2,
       price: 35000,
     },
     {
@@ -144,7 +161,7 @@ Room.create(
       title: '湘南の海が一望できる別荘で素敵な休日を過ごしませんか？',
       registration_id: 'M0000003',
       body: body,
-      image_url: 'https://ateamateam.herokuapp.com/images/sample_room.jpg',
+      image_url: 'https://ateamateam.herokuapp.com/images/room3.jpg',
       prefecture: '神奈川県',
       city: '箱根',
       latitude: 35.239728,
@@ -168,7 +185,7 @@ Room.create(
       title: '湘南の海が一望できる別荘で素敵な休日を過ごしませんか？',
       registration_id: 'M0000004',
       body: body,
-      image_url: 'https://ateamateam.herokuapp.com/images/sample_room.jpg',
+      image_url: 'https://ateamateam.herokuapp.com/images/room4.jpg',
       prefecture: '神奈川県',
       city: '箱根',
       latitude: 35.239728,
@@ -192,7 +209,7 @@ Room.create(
       title: '湘南の海が一望できる別荘で素敵な休日を過ごしませんか？',
       registration_id: 'M0000005',
       body: body,
-      image_url: 'https://ateamateam.herokuapp.com/images/sample_room.jpg',
+      image_url: 'https://ateamateam.herokuapp.com/images/room1.jpg',
       prefecture: '神奈川県',
       city: '箱根',
       latitude: 35.239728,
@@ -216,7 +233,7 @@ Room.create(
       title: '湘南の海が一望できる別荘で素敵な休日を過ごしませんか？',
       registration_id: 'M0000006',
       body: body,
-      image_url: 'https://ateamateam.herokuapp.com/images/sample_room.jpg',
+      image_url: 'https://ateamateam.herokuapp.com/images/room2.jpg',
       prefecture: '神奈川県',
       city: '箱根',
       latitude: 35.239728,
@@ -240,7 +257,7 @@ Room.create(
       title: '湘南の海が一望できる別荘で素敵な休日を過ごしませんか？',
       registration_id: 'M0000007',
       body: body,
-      image_url: 'https://ateamateam.herokuapp.com/images/sample_room.jpg',
+      image_url: 'https://ateamateam.herokuapp.com/images/room3.jpg',
       prefecture: '神奈川県',
       city: '箱根',
       latitude: 35.239728,
